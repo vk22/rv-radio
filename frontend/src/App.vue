8e91c856384522867dff03dda8dd6c92d899818c
victor c <v.kushnir22@gmail.com>
2026-06-30 22:57:03 +0300

    logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@ import { RouterView } from 'vue-router'
 
 <template>
   <header class="fixed left-0 top-0 z-[999] flex w-screen items-center justify-center px-2 py-2 font-sans">
-    <img class="block w-full" src="@/assets/logo2.svg"/>
+    <img class="block w-full opacity-40" src="@/assets/logo2.svg"/>
   </header>
   <RouterView />
 </template>
